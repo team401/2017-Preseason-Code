3a9500d15a7c1253fe3d32537cc38507b8df7278
Zachary Kozar <zachkozar@masterzach32.net>
2016-11-09 17:04:20 -0500

    Created TestBot project with ToastAPI

---
+++ TestBot/src/main/java/frc/team401/testbot/RobotModule.java
@@ -0,0 +1,26 @@
+package frc.team401.testbot;
+
+import jaci.openrio.toast.lib.log.Logger;
+import jaci.openrio.toast.lib.module.IterativeModule;
+
+public class RobotModule extends IterativeModule {
+
+    public static Logger logger;
+
+    @Override
+    public String getModuleName() {
+        return "TestBot";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+    @Override
+    public void robotInit() {
+        logger = new Logger("TestBot", Logger.ATTR_DEFAULT);
+        logger.info("Help me Preston");
+        //TODO: Module Init
+    }
+}
