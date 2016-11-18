6f625a71bbc4ee75b3c426ca937fdfccb4c094e9
Liam Lawrence <and3212@gmail.com>
2016-11-18 10:19:24 -0500

    Trying to fix my mess

---
+++ Vision/main.cpp
@@ -0,0 +1,41 @@
+/* Author: Liam Lawrence
+ * Date: 11.14.16
+ *
+ * Vision file written in VIM to show Cameron
+ * that he needs to actually install OpenCV
+ */
+
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(){
+
+    VideoCapture cap;
+    Mat frame;
+
+    if(!cap.open(0))
+        return 0;
+
+
+    //cap.set(CV_CAP_PROP_FRAME_WIDTH,320);
+    //cap.set(CV_CAP_PROP_FRAME_HEIGHT,240);
+    cap.set(CV_CAP_PROP_FRAME_WIDTH,176);
+    cap.set(CV_CAP_PROP_FRAME_HEIGHT,144);
+
+    for(;;){
+        // Passes camera cap to frame
+        cap >> frame;
+
+        // If the frame is blank or we hit ESC, break
+        if(frame.empty() || waitKey(1) == 27)
+            break;
+
+
+        // Creates the GUI for us
+        namedWindow("video", WINDOW_AUTOSIZE);
+        imshow("video", frame);
+    }
+}
