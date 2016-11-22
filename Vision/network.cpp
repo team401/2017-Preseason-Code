02ae3cd5350e54604f72356f35737e589201792d
Cameron Earle <camearle20@me.com>
2016-11-21 20:08:28 -0500

    Added a test class

---
+++ Vision/network.cpp
@@ -0,0 +1,13 @@
+//
+// Created by cameronearle on 11/21/16.
+//
+
+#include "network.h"
+
+network::network(int testInt_s) {
+    testInt = testInt_s;
+}
+
+int network::getTestInt() {
+    return testInt;
+}
---
+++ Vision/network.h
@@ -0,0 +1,16 @@
+//
+// Created by cameronearle on 11/21/16.
+//
+
+#ifndef INC_2017_PRESEASON_CODE_NETWORK_H
+#define INC_2017_PRESEASON_CODE_NETWORK_H
+
+
+struct network {
+    int testInt;
+    network(int testInt_s);
+    int getTestInt();
+};
+
+
+#endif //INC_2017_PRESEASON_CODE_NETWORK_H
