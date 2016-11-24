9731bd688b75a5d69efe47b2791bbdf0a7bb9482
Cameron Earle <camearle20@me.com>
2016-11-24 13:15:11 -0500

    Added contour detection

--- Vision/CannyDetector.cpp
+++ Vision/CannyDetector.cpp
@@ -7,13 +7,18 @@
 #include "opencv2/opencv.hpp"
 
 using namespace cv;
+using namespace std;
 
 void CannyDetector::run() {
     Mat frame; //Set up a Mat for the image from the camera
     Mat edges; //Set up a Mat for the canny image
+    Mat contoursMat; //Set up a Mat for the contour image
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
 
     namedWindow("Original", WINDOW_AUTOSIZE); //Create a window for the original image
     namedWindow("Canny", WINDOW_AUTOSIZE); //Create a window for the canny image
+    namedWindow("Contours", WINDOW_AUTOSIZE); //Create a window for showing contours
 
     for (;;) {
         if(waitKey(1) == 27) { //If ESC is pressed, break the loop
@@ -26,7 +31,14 @@ void CannyDetector::run() {
 
         Canny(edges, edges, thresh1, thresh2); //Get canny image and write the data to it
 
+        findContours( edges, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) ); //Find the contours
+
+        contoursMat = Mat::zeros( edges.size(), CV_8UC3 ); //Initialize the contour Mat
+
+        cv::drawContours (contoursMat, contours, -1, cv::Scalar (0, 0, 255), 2); //Draw the contours
+
         imshow("Original", frame); //Write the original frame to the Original window
         imshow("Canny", edges); //Write the canny frame to the Canny window
+        imshow("Contours", contoursMat);
     }
 }
