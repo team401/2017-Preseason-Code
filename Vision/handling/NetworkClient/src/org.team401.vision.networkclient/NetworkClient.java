be82bc106797b1211bffaa2b5b1b33faf216e288
Cameron Earle <camearle20@me.com>
2016-12-04 19:11:47 -0500

    Added handling framework for java development

---
+++ Vision/handling/NetworkClient/src/org.team401.vision.networkclient/NetworkClient.java
@@ -0,0 +1,7 @@
+package org.team401.vision.networkclient;
+
+class NetworkClient {
+    public static void main(String[] args) {
+        System.out.println("Hello from an acutally good language");
+    }
+}
