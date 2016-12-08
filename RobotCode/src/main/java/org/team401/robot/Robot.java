6f911d18b7cc5f4667431bbd1c54b7fcc324a9f2
Unknown <Ltagg7@gmail.com>
2016-12-07 19:22:54 -0500

    Made working drive code :)

--- RobotCode/src/main/java/org/team401/robot/Robot.java
+++ RobotCode/src/main/java/org/team401/robot/Robot.java
@@ -1,60 +1,41 @@
-/*
-    Quetzalcoatl - Copperhead Robotics 2016 Robot code for FIRST Stronghold
-    Copyright (C) 2016 FRC Team 401
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
 package org.team401.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 import org.strongback.Strongback;
+import org.strongback.components.Motor;
+import org.strongback.components.Solenoid;
+import org.strongback.components.ui.FlightStick;
+import org.strongback.drive.TankDrive;
+import org.strongback.hardware.Hardware;
 
 public class Robot extends IterativeRobot {
-
-    /*
-        For this assignment, you need to get the 2016 robot completely driving in
-        Java, and be able to switch between low and high gear. You should create
-        a new branch off of this one in GitKraken, name it "yourname-drive" without
-        quotes, and push it to GitHub for Liam and I to review by this Saturday, 12/10.
-            - Zach
-     */
+    private Motor leftGearbox, rightGearbox;
+    private Solenoid solenoid;
+    private FlightStick leftJoystick, rightJoystick;
+    private TankDrive drive;
 
     @Override
     public void robotInit() {
+
         Strongback.configure()
                 .recordDataToFile("/home/lvuser/")
                 .recordEventsToFile("/home/lvuser/", 2097152);
+        leftGearbox = Motor.compose(
+                Hardware.Motors.talonSRX(0),
+                Hardware.Motors.talonSRX(1).invert(),
+                Hardware.Motors.talonSRX(2).invert());
 
-        /*
-            Initialize your motors here. The gearboxes on the 2016 bot each have 3 motors.
-            The front and rear motors spin one direction while the middle will spin in the opposite.
-            You need to either log into the robot via 10.40.1.2 when your at the shop or
-            look at the motors on the robot to get their CAN IDs. We use TalonSRX.
-         */
+        rightGearbox = Motor.compose(
+                Hardware.Motors.talonSRX(5),
+                Hardware.Motors.talonSRX(6),
+                Hardware.Motors.talonSRX(7).invert());
+        drive = new TankDrive(leftGearbox, rightGearbox);
 
-        // solenoid initialization
+        leftJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
+        rightJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(1);
 
-        /*
-            Initialize the solenoid here. You only need one solenoid as it controls
-            the piston in both gearboxes.
-         */
-
-        // solenoid initialization
-
-        // joysticks, LogitechAttack3D
+        solenoid = Hardware.Solenoids.doubleSolenoid(4, 6, Solenoid.Direction.RETRACTING);
 
     }
 
@@ -70,7 +51,13 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
-        // here is where you should read joystick inputs and set motor speeds
+        drive.tank(leftJoystick.getPitch().read(), rightJoystick.getPitch().read());
+
+        if (leftJoystick.getButton(2).isTriggered()){
+            solenoid.extend();
+        }else if(leftJoystick.getButton(3).isTriggered()){
+            solenoid.retract();
+        }
     }
 
     @Override
