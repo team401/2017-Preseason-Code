233da6556ac3959187bb36beab37fd8820798b03
Unknown <cased@vt.edu>
2016-12-19 22:27:24 -0500

    possible gyro

--- RobotCode/src/main/java/org/team401/robot/Robot.java
+++ RobotCode/src/main/java/org/team401/robot/Robot.java
@@ -1,70 +1,62 @@
-/*
-    Quetzalcoatl - Copperhead Robotics 2016 Robot code for FIRST Stronghold
-    Copyright (C) 2016 FRC Team 401
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+/* Created Sat Dec 17 19:36:59 EST 2016 */
 package org.team401.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 import org.strongback.Strongback;
 import org.strongback.components.Motor;
-import org.strongback.components.Solenoid;
 import org.strongback.components.ui.FlightStick;
+import org.strongback.drive.TankDrive;
 import org.strongback.hardware.Hardware;
 
+
 public class Robot extends IterativeRobot {
+    private Motor leftGearbox, rightGearbox;
+    private FlightStick leftJoystick, rightJoystick;
+    private TankDrive drive;
 
     @Override
     public void robotInit() {
         Strongback.configure()
                 .recordDataToFile("/home/lvuser/")
                 .recordEventsToFile("/home/lvuser/", 2097152);
+    	/*leftGearbox=Motor.compose(
+    			Hardware.Motors.talonSRX(0).invert(),
+    			Hardware.Motors.talonSRX(1).invert(),
+    			Hardware.Motors.talonSRX(2));
+    	rightGearbox=Motor.compose(
+    			Hardware.Motors.talonSRX(3),
+    			Hardware.Motors.talonSRX(4),
+    			Hardware.Motors.talonSRX(5).invert());
+    	*/
+        leftGearbox=Hardware.Motors.talon(0);
+        rightGearbox=Hardware.Motors.talon(1);
+        drive = new TankDrive(leftGearbox, rightGearbox);
 
-        // initializing a motor
-        Motor m = Hardware.Motors.talonSRX(1);
+        leftJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
+        rightJoystick = Hardware.HumanInterfaceDevices.logitechAttack3D(1);
 
-        // initializing a solenoid
-        Solenoid s = Hardware.Solenoids.doubleSolenoid(0, 1, Solenoid.Direction.RETRACTING);
 
-        FlightStick joystick = Hardware.HumanInterfaceDevices.logitechAttack3D(0);
     }
 
     @Override
     public void teleopInit() {
+        // Start Strongback functions ...
         Strongback.restart();
-    }
 
-    @Override
-    public void autonomousInit() {
-        Strongback.start();
     }
 
     @Override
     public void teleopPeriodic() {
-        // do stuff when robot is enabled
-    }
 
-    @Override
-    public void autonomousPeriodic() {
-
+        drive.tank(leftJoystick.getPitch().read(),rightJoystick.getPitch().read());
     }
 
     @Override
     public void disabledInit() {
+        // Tell Strongback that the robot is disabled so it can flush and kill commands.
         Strongback.disable();
     }
+
 }
+
