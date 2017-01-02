4fbdd066daa277e85bea78a6570a46058e7a92a2
Neema <neemaahm@gmail.com>
2017-01-02 16:38:30 -0500

    Added ramping code to the gyro code.  Ramping code only works when the user isn't using the gyro code because there is no need for it.

--- RobotCode/src/main/java/org/team401/robot/Robot.java
+++ RobotCode/src/main/java/org/team401/robot/Robot.java
@@ -1,17 +1,14 @@
 /*
     Quetzalcoatl - Copperhead Robotics 2016 Robot code for FIRST Stronghold
     Copyright (C) 2016 FRC Team 401
-
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -28,7 +25,6 @@ import org.strongback.components.ui.FlightStick;
 import org.strongback.drive.TankDrive;
 import org.strongback.hardware.Hardware;
 
-
 public class Robot extends IterativeRobot {
 
     private FlightStick joysticky;
@@ -40,6 +36,7 @@ public class Robot extends IterativeRobot {
     private double pitch;
     private double turnSpeed;
     private double throttle = 0.4;
+    private PIDController pidController;
 
     @Override
     public void robotInit() {
@@ -81,20 +78,11 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         //kp = SmartDashboard.getNumber("kp");
         if (!joysticky.getTrigger().isTriggered()) {
-            double accelerationDivident;
-            accelerationDivident = 10 * joysticky.getThrottle().invert().read();
             pitch = joysticky.getPitch().read();
-            turnSpeed = joysticky.getRoll().invert().read();
-            if (pitch < 0.1) {
-                throttle = throttle + Math.pow((pitch - throttle)/accelerationDivident, 2);
-                allDrive.arcade(pitch*throttle, joysticky.getRoll().read());
-            }
-            if (pitch > 0.1 || pitch == 0.1) {
-                allDrive.stop();
-                throttle = 0.4;
-            }
+            turnSpeed = joysticky.getRoll().read();
+            allDrive.arcade(pitch, turnSpeed * 0.9);
         }
-        if (joysticky.getTrigger().isTriggered()){
+        else {
             double angle = myGyro.getAngle();
             allDrive.arcade(joysticky.getPitch().read()*0.8, -angle*kp);
             SmartDashboard.putNumber("Angle: ", angle);
